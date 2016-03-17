51934d2421b8cc3301e89d969977f2185fd751c1
ee-psharbaugh <psharbaugh@etcedu.com>
2016-03-17 16:43:15 -0400

    Fixed shadows

--- omelet/src/components/ui/omelet.ui.rectangle.js
+++ omelet/src/components/ui/omelet.ui.rectangle.js
@@ -75,7 +75,7 @@ omelet.egg('omelet.ui.rectangle', function(data, refs) {
 
             context.fill();
         }
-        if (this.strokeStyle) {
+        if (!shadowDraw && this.strokeStyle) {
             context.lineWidth = this.strokeWidth;
             context.strokeStyle = this.strokeStyle;
             context.stroke();
