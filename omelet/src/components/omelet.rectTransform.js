9336534ca1fe247b735f95973ac3273a67b469e2
ee-psharbaugh <psharbaugh@etcedu.com>
2016-03-18 14:58:44 -0400

    Setup up rect transform to have min / max sizes

--- omelet/src/components/omelet.rectTransform.js
+++ omelet/src/components/omelet.rectTransform.js
@@ -1,19 +1,46 @@
 omelet.egg('omelet.rectTransform', function(data, refs) {
-    this.left = data.left;
-    this.top = data.top;
-    this.right = data.right;
-    this.bottom = data.bottom;
-    this.offsetLeft = data.offsetLeft;
-    this.offsetTop = data.offsetTop;
-    this.offsetRight = data.offsetRight;
-    this.offsetBottom = data.offsetBottom;
+    this.left = data.left;  // \
+    this.top = data.top;    //  \       this code
+    this.right = data.right; //  \_
+    this.bottom = data.bottom; //  \_____    looks like it
+    this.offsetLeft = data.offsetLeft; //\
+    this.offsetTop = data.offsetTop;   // \      would be a fun hill
+    this.offsetRight = data.offsetRight;// \
+    this.offsetBottom = data.offsetBottom;//\___
+    this.minWidthPixels = data.minWidthPixels;//\_        to roll down.
+    this.minHeightPixels = data.minHeightPixels;//\
+    this.maxWidthPercent = data.maxWidthPercent; //|
+    this.maxHeightPercent = data.maxHeightPercent;//\
     
     this.transform = function(wrapper) {
         var rect = wrapper.getRect();
-        var x = (this.left * rect[2]) + this.offsetLeft;
-        var y = (this.top * rect[3]) + this.offsetTop;
-        var w = (this.right * rect[2]) -  this.offsetRight - x;
-        var h = (this.bottom * rect[3]) - this.offsetBottom - y;
+        var parentWidth = rect[2];
+        var parentHeight = rect[3];
+
+        var x = (this.left * parentWidth) + this.offsetLeft;
+        var y = (this.top * parentHeight) + this.offsetTop;
+        var w = (this.right * parentWidth) -  this.offsetRight - x;
+        var h = (this.bottom * parentHeight) - this.offsetBottom - y;
+
+        if (w < this.minWidthPixels) {
+            x -= (this.minWidthPixels - w) / 2;
+            w = this.minWidthPixels;
+        }
+        if (h < this.minHeightPixels) {
+            y -= (this.minHeightPixels - h) / 2;
+            h = this.minHeightPixels;
+        }
+        if (parentWidth != 0 && (w / parentWidth) > this.maxWidthPercent) {
+            var dw = this.maxWidthPercent * parentWidth;
+            x += (w - dw) / 2;
+            w = dw;
+        }
+        if (parentHeight != 0 && (h / parentHeight) > this.maxHeightPercent) {
+            var dh = this.maxHeightPercent * parentHeight;
+            y += (h - dh) / 2;
+            h = dh;
+        }
+
         rect = [rect[0] + x, rect[1] + y, w, h];
         wrapper.setRect(rect);
     };
@@ -25,5 +52,9 @@ omelet.egg('omelet.rectTransform', function(data, refs) {
     offsetLeft:10,
     offsetTop:10,
     offsetRight:10,
-    offsetBottom:10
+    offsetBottom:10,
+    minWidthPixels:0,
+    minHeightPixels:0,
+    maxWidthPercent:2,
+    maxHeightPercent:2
 });
